1641d5e7b67ff6b40b1734d4ff3441d9791769a8
Amigo-1234 <olamidesamson841@gmail.com>
2026-03-04 03:36:26 +0100

    hamburger fixed

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("navLinks");
 
 hamburger.addEventListener("click", () => {
-  if (navLinks.style.display === "flex") {
-    navLinks.style.display = "none";
-  } else {
-    navLinks.style.display = "flex";
-  }
+  navLinks.classList.toggle("active");
+  hamburger.classList.toggle("active");
 });
